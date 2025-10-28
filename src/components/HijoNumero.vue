29d6523c5ba2306d2b511e5b997118fa9e35d872
mmtol <mmigueltolosana@gmail.com>
2025-10-28 10:33:31 +0100

    aaaa

--- src/components/HijoNumero.vue
+++ src/components/HijoNumero.vue
@@ -1,13 +1,22 @@
 <template>
     <div>
-
+        <h1>Numero: {{ num }}</h1>
+        <button @click="sumarNum">Sumar {{ num }}</button>
     </div>
 </template>
 
 <script>
     export default 
     {
-        
+        name:"HijoNumero",
+        props:["num"],
+        methods:
+        {
+            sumarNum()
+            {
+                this.$emit("sumarNum", this.num);
+            }
+        }
     }
 </script>
 
--- src/components/PadreNumeros.vue
+++ src/components/PadreNumeros.vue
@@ -4,16 +4,42 @@
         <h2>La suma es: {{suma}}</h2>
         <button @click="crearHijo">Nuevo Número</button>
         <hr/>
+        <ul>
+            <li v-for="num in nums" :key="num">
+                <HijoNumero :num="num" v-on:sumarNum="sumarNum"/>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import HijoNumero from './HijoNumero.vue';
+
     export default 
     {
         name:"PadreDeportes",
+        components:
+        {
+            HijoNumero
+        },
         data()
         {
-            
+            return{
+                suma:0,
+                nums:[]
+            }
+        },
+        methods:
+        {
+            crearHijo()
+            {
+               var num = parseInt(Math.random() * 100) + 1;
+               this.nums.push(num);
+            },
+            sumarNum(num)
+            {
+                this.suma+=num;
+            }
         }
     }
 </script>
